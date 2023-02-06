566e0a7ddb137a5d1cb0abdf5e776dd8d4bba9f7
laoadr <laoadr@gmail.com>
2023-02-06 15:33:52 -0800

    added a container

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather Application</h1>
       <footer>This project was coded by Adrienne Lao and is open-sourced on <em><a href="https://github.com/laoadr/weather-app" target="_blank">Github</a></em></footer>
+      </div>
     </div>
   );
 }
