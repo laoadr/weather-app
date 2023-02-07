9b93e626ddf1edea51ad952a085550189f76e3b9
laoadr <laoadr@gmail.com>
2023-02-06 16:02:29 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Weather from "./Weather";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
       <h1>Weather Application</h1>
+      <Weather />
       <footer>This project was coded by Adrienne Lao and is open-sourced on <a href="https://github.com/laoadr/weather-app" target="_blank">Github</a></footer>
-            </div>
+      </div>
     </div>
   );
 }
